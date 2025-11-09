b19aa92a74d50feb00c1b07282525abe9415a2b9
Agustin Arnez <itsmeagus2@gmail.com>
2025-11-09 08:57:39 -0400

    nolose

--- Assets/Scripts/Menu/MenuScript.cs
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -1,16 +1,15 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuSystem : MonoBehaviour
+public class MenuScript : MonoBehaviour
 {
     public void Jugar()
     {
-        SceneManager.LoadScene("Nivel1");
+        SceneManager.LoadScene("nivel 1");
     }
-    public void Salir()
+     public void Salir()
     {
         Debug.Log("Saliendo del juego...");
-        Application.Quit();
+        Application.Quit(); 
     }
 }
